8af60685381170ed3dba9b3f2f91b42eff49a67f
Arthur <zhenya.brovcko@yandex.ru>
2018-11-03 16:01:29 +0200

    fixed tests

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -17,23 +17,23 @@ describe Parser do
     end
   end
 
-  # describe '#fetch_movie' do
-  #   let(:movie_id_with_budget) { 'tt0068646' }
-  #   let(:movie_id_without_budget) { 'tt0022100' }
-  #   let(:non_existent_movie_id) { 'tt9999999' }
+  describe '#fetch_movie' do
+    let(:movie_id_with_budget) { 'tt0068646' }
+    let(:movie_id_without_budget) { 'tt0022100' }
+    let(:non_existent_movie_id) { 'tt9999999' }
 
-  #   context 'movie_id_with_budget' do
-  #     subject { parser.fetch_movie(movie_id_with_budget) }
-  #     it { is_expected.to include(ru_title: 'Крестный отец', poster: /\.jpg$/i, budget: '6000000') }
-  #   end
+    context 'movie_id_with_budget' do
+      subject { parser.fetch_movie(movie_id_with_budget) }
+      it { is_expected.to include(ru_title: 'Крестный отец', poster: /\.jpg$/i, budget: '6000000') }
+    end
 
-  #   context 'movie_id_without_budget' do
-  #     subject { parser.fetch_movie(movie_id_without_budget) }
-  #     it { is_expected.to include(ru_title: 'М убийца', poster: /\.jpg$/i, budget: 'N/A') }
-  #   end
+    context 'movie_id_without_budget' do
+      subject { parser.fetch_movie(movie_id_without_budget) }
+      it { is_expected.to include(ru_title: 'М убийца', poster: /\.jpg$/i, budget: 'N/A') }
+    end
 
-  #   it 'non_existent_movie_id' do
-  #     expect{ parser.fetch_movie(non_existent_movie_id) }.to raise_error(Parser::MovieNotFound, '404 Not Found')
-  #   end
-  # end
+    it 'non_existent_movie_id' do
+      expect{ parser.fetch_movie(non_existent_movie_id) }.to raise_error(Parser::MovieNotFound, '404 Not Found')
+    end
+  end
 end
