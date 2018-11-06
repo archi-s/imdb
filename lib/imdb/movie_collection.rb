fc174223d2672de5e60533a20c7353d62963c2e7
Arthur <zhenya.brovcko@yandex.ru>
2018-11-06 15:20:34 +0200

    Added YARD documentation

--- lib/imdb/movie_collection.rb
+++ lib/imdb/movie_collection.rb
@@ -34,7 +34,7 @@ module Imdb
       @movies.flat_map(&:genre).uniq
     end
     # Returns collection of movies sorted by options.
-    # @param arg [Symbol]
+    # @param options [Symbol]
     # @return [Array] sorted movies list.
     def sort_by(options)
       check_options!(*options)
@@ -46,7 +46,7 @@ module Imdb
       @movies.map(&:director).uniq.sort_by { |director| director.split(' ').last }
     end
     # Number of films produced not in %production%.
-    # @param arg [String]
+    # @param production [String]
     # @return [Fixnum]
     # @example Get director stats.
     #   "collection.not_country_movies('USA')" #=> 84
@@ -55,7 +55,7 @@ module Imdb
     end
     # This method returns count release of movies sorted by month
     # @return [Array]
-    # @example.
+    # @example
     #   "collection.stat_by_month" #=> ["DECEMBER"=>10]
     def stat_by_month
       @movies.reject { |movie| movie.release.count('-').zero? }
@@ -66,13 +66,13 @@ module Imdb
     end
     # Lets you to filter collection by movie arguments.
     # Multiple filters are allowed.
-    # @param filters [Array<Hash>] List of filters.
+    # @param options [Array<Hash>] List of filters.
     def filter(options)
       check_options!(*options.keys)
       all.select { |movie| movie.matches_filter?(options) }
     end
     # This method counts movies attribute values in collection by given attribute name.
-    # @param arg [Symbol]
+    # @param options [Symbol]
     # @return [Hash]
     # @example Get director stats.
     #   "movie.stats(:director)" #=> "{"Frank Darabont"=>2, ...}"
@@ -83,7 +83,7 @@ module Imdb
 
     private
     # Checking options for compliance with specified keys
-    # @param arg [Hash]
+    # @param options [Hash]
     # @return [nil] or raise error
     def check_options!(*options)
       keys = all.sample.methods + [:exclude_country]
